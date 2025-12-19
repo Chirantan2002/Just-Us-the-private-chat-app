fe5eb48096de27b6d492d0ca7caf506cbc9a3720
Chirantan <chirantanbiswas2017@gmail.com>
2025-12-19 22:25:19 +0530

    Uploading project after finishing on 19/12/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,165 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import DotGrid from "@/components/DotGrid";
+import { Monda } from "next/font/google";
+import { useUsername } from "@/hooks/use-username";
+import { client } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
+import { HiBolt } from "react-icons/hi2";
+import { GoAlertFill } from "react-icons/go";
+import { Archivo_Black } from "next/font/google";
+
+const archivoBlack = Archivo_Black({
+  variable: "--font-archivo-black",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const monda = Monda({
+  variable: "--font-monda",
+  subsets: ["latin"],
+});
+
+const Page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <Suspense>
+      <Lobby />
+    </Suspense>
+  );
+};
+
+export default Page;
+
+function Lobby() {
+  const { username } = useUsername();
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const wasDestroyed = searchParams.get("destroyed") === "true";
+  const error = searchParams.get("error");
+  const [showMessage, setShowMessage] = useState(true);
+
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post();
+
+      if (res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (wasDestroyed || error) {
+      setShowMessage(true);
+
+      const timer = setTimeout(() => {
+        setShowMessage(false);
+        router.replace("/"); // optional: clears query params
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [wasDestroyed, error, router]);
+
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-center bg-[#000524]"
+      style={{ width: "100%", height: "100vh", position: "relative" }}
+    >
+      {/* dot grid bg  */}
+      <DotGrid
+        dotSize={10}
+        gap={15}
+        baseColor="#033270"
+        activeColor="#5227FF"
+        proximity={120}
+        shockRadius={250}
+        shockStrength={5}
+        resistance={750}
+        returnDuration={1.5}
+        className="opacity-50 blur-[2px]"
+      />
+
+      <div className="absolute w-full max-w-md space-y-8 px-4">
+        {showMessage && wasDestroyed && (
+          <div className="bg-red-950/50 border rounded-lg border-red-900 p-4 text-center transition-opacity duration-500">
+            <span className="text-red-500 text-sm font-bold flex items-center justify-center gap-2">
+              <GoAlertFill /> Room Destroyed...
+            </span>
+            <p className="text-zinc-100 text-xs mt-1">
+              All messages were permanently deleted.
+            </p>
+          </div>
+        )}
+        {showMessage && error === "room-not-found" && (
+          <div className="bg-red-950/50 border rounded-lg border-red-900 p-4 text-center transition-opacity duration-500">
+            <p className="text-red-500 text-sm font-bold flex items-center justify-center gap-2">
+              <GoAlertFill /> Room Not Found...
+            </p>
+            <p className="text-zinc-100 text-xs mt-1">
+              This room may have expired or never existed.
+            </p>
+          </div>
+        )}
+        {showMessage && error === "room-full" && (
+          <div className="bg-red-950/50 border rounded-lg border-red-900 p-4 text-center transition-opacity duration-500">
+            <p className="text-red-500 text-sm font-bold flex items-center justify-center gap-2">
+              <GoAlertFill /> Room is Full...
+            </p>
+            <p className="text-zinc-100 text-xs mt-1">
+              This room is at maximum capacity.
+            </p>
+          </div>
+        )}
+
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="bg-black/10 backdrop-blur border border-[#c6f91f] rounded-full px-2 py-1">
+            <span
+              className={`${monda.className}  flex items-center justify-center gap-2 font-bold uppercase text-[10px] md:text-sm text-[#c6f91f]`}
+            >
+              <HiBolt className="animate-pulse rotate-15" />
+              welcome
+            </span>
+          </div>
+          <h1
+            className={`${archivoBlack.className} text-3xl md:text-5xl font-bold tracking-tight leading-tight text-[#c6f91f] m-4 uppercase`}
+          >
+            it&apos;s just us!
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+        </div>
+
+        <div className="text-center space-y-2">
+          <p className="text-[#c6f91f] text-xs md:text-[15px]">
+            Private chat, just for two...
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="border border-[#c6f91f]/50 rounded-lg bg-white/10 p-6 m-4 backdrop-blur-[5px]">
+          <div className="space-y-5">
+            <div className="space-y-2">
+              <label className="flex items-center justify-center text-[#c6f91f]">
+                Your Identity...
+              </label>
+
+              <div className="flex items-center gap-3">
+                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-[#8eadc4] font-mono rounded-lg">
+                  {username}
+                </div>
+              </div>
+            </div>
+
+            <button
+              onClick={() => createRoom()}
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-neutral-950 hover:text-[#c6f91f] transition-colors duration-200 mt-2 cursor-pointer disabled:opacity-50 rounded-lg"
+            >
+              CREATE SECURE ROOM
+            </button>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
---
+++ src/app/room/[roomId]/page.tsx
@@ -0,0 +1,230 @@
+"use client";
+
+import { useUsername } from "@/hooks/use-username";
+import { client } from "@/lib/client";
+import { useRealtime } from "@/lib/realtime-client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+
+function formatTimeRemaining(seconds: number) {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${mins}:${secs.toString().padStart(2, "0")}`;
+}
+
+const Page = () => {
+  const params = useParams();
+  const roomId = params.roomId as string;
+
+  const router = useRouter();
+
+  const { username } = useUsername();
+  const [input, setInput] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [copyStatus, setCopyStatus] = useState("COPY LINK");
+  const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+
+  const { data: ttlData } = useQuery({
+    queryKey: ["ttl", roomId],
+    queryFn: async () => {
+      const res = await client.room.ttl.get({ query: { roomId } });
+      return res.data;
+    },
+  });
+
+  useEffect(() => {
+    if (ttlData?.ttl !== undefined) setTimeRemaining(ttlData.ttl);
+  }, [ttlData]);
+
+  useEffect(() => {
+    if (timeRemaining === null || timeRemaining < 0) return;
+
+    if (timeRemaining === 0) {
+      router.push("/?destroyed=true");
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeRemaining((prev) => {
+        if (prev === null || prev <= 1) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeRemaining, router]);
+
+  const { data: messages, refetch } = useQuery({
+    queryKey: ["messages", roomId],
+    queryFn: async () => {
+      const res = await client.messages.get({ query: { roomId } });
+      return res.data;
+    },
+  });
+
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: async ({ text }: { text: string }) => {
+      await client.messages.post(
+        { sender: username, text },
+        { query: { roomId } }
+      );
+
+      setInput("");
+    },
+  });
+
+  useRealtime({
+    channels: [roomId],
+    events: ["chat.message", "chat.destroy"],
+    onData: ({ event }) => {
+      if (event === "chat.message") {
+        refetch();
+      }
+
+      if (event === "chat.destroy") {
+        router.push("/?destroyed=true");
+      }
+    },
+  });
+
+  const { mutate: destroyRoom } = useMutation({
+    mutationFn: async () => {
+      await client.room.delete(null, { query: { roomId } });
+    },
+  });
+
+  const copyLink = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+    setCopyStatus("COPIED!");
+    setTimeout(() => setCopyStatus("COPY LINK"), 2000);
+  };
+
+  return (
+    <main className="flex flex-col h-screen max-h-screen max-w-4xl mx-auto md:border md:border-zinc-800 overflow-hidden">
+      <header className="border-b border-zinc-800 flex items-center justify-between bg-neutral-900/30 flex-col md:flex-row">
+        <div className="flex items-center gap-4 border-b border-zinc-800 md:border-none">
+          <div className="flex flex-col p-4 md:border-r md:border-zinc-800">
+            <span className="text-xs text-zinc-500 uppercase">Room ID:</span>
+            <div className="flex items-center gap-2">
+              <span className="font-bold text-green-500 truncate">
+                {roomId}
+              </span>
+              <button
+                onClick={copyLink}
+                className="text-[10px] bg-zinc-800 hover:bg-zinc-700 px-2 py-0.5 rounded text-zinc-400 hover:text-zinc-200 transition-colors"
+              >
+                {copyStatus}
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-col p-4">
+            <span className="text-xs text-zinc-500 uppercase">
+              Self-Destruct countdown:
+            </span>
+            <span
+              className={`md:text-lg font-bold flex items-center gap-2 ${
+                timeRemaining !== null && timeRemaining < 60
+                  ? "text-red-500"
+                  : "text-amber-500"
+              }`}
+            >
+              {timeRemaining !== null
+                ? formatTimeRemaining(timeRemaining)
+                : "--:--"}
+            </span>
+          </div>
+        </div>
+
+        <div className="p-4 flex items-center justify-center">
+          <button
+            onClick={() => destroyRoom()}
+            className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center gap-2 disabled:opacity-50 uppercase"
+          >
+            <span className="group-hover:animate-pulse">💣</span>
+            DESTROY ROOM
+          </button>
+        </div>
+      </header>
+
+      {/* MESSAGES */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin">
+        {messages?.messages.length === 0 && (
+          <div className="flex items-center justify-center h-full">
+            <p className="text-zinc-600 text-sm font-mono">
+              No messages yet, start the conversation.
+            </p>
+          </div>
+        )}
+
+        {messages?.messages.map((msg) => (
+          <div key={msg.id} className="flex flex-col items-start">
+            <div className="max-w-[80%] group">
+              <div className="flex items-baseline gap-3 mb-1">
+                <span
+                  className={`text-xs font-bold ${
+                    msg.sender === username ? "text-green-400" : "text-blue-300"
+                  }`}
+                >
+                  {msg.sender === username ? "YOU" : msg.sender}
+                </span>
+
+                <span className="text-[10px] text-zinc-600">
+                  {format(msg.timestamp, "HH:mm")}
+                </span>
+              </div>
+
+              <p className="text-sm text-zinc-300 leading-relaxed break-all">
+                {msg.text}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+              {">"}
+            </span>
+            <input
+              autoFocus
+              type="text"
+              value={input}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && input.trim()) {
+                  sendMessage({ text: input });
+                  inputRef.current?.focus();
+                }
+              }}
+              placeholder="Type message..."
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm rounded-lg"
+            />
+          </div>
+
+          <button
+            onClick={() => {
+              sendMessage({ text: input });
+              inputRef.current?.focus();
+            }}
+            disabled={!input.trim() || isPending}
+            className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer rounded-lg"
+          >
+            SEND
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
